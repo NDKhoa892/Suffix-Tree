574a144381e37fcff4995e1d635efbed3044cebd
NDKhoa892 <harrynguyen.origami@gmail.com>
2021-07-14 20:44:28 +0700

    Initial Commit

---
+++ Suffix Tree/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Suffix Tree
+//
+//  Created by Harry Nguyen on 14/07/2021.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
